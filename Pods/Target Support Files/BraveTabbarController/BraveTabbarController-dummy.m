7d69833a7f1001cdc1b02d0ea967d24bfb144e31
Hien Pham <ngochiencse@gmail.com>
2023-02-28 10:52:00 +0700

    Remove Pods source code folder

--- Pods/Target Support Files/BraveTabbarController/BraveTabbarController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BraveTabbarController : NSObject
-@end
-@implementation PodsDummy_BraveTabbarController
-@end
--- Pods/Target Support Files/ImageRow/ImageRow-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ImageRow : NSObject
-@end
-@implementation PodsDummy_ImageRow
-@end
